469649abe9f9ad43ee4d69d344b9ca1bd2d3eb05
Ujjval Yadav <ujjvalyadav746@gmail.com>
2026-04-02 15:41:33 +0530

    conf, auth

---
+++ megaBlogProject/src/appwrite/auth.js
@@ -0,0 +1,56 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    Client = new Client();
+    account;
+
+    constructor() {
+        this.Client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId);
+        this.account = new Account(this.Client);
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                return this.login({email, password});
+            }else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            const session = await this.account.createEmailPasswordSession(email, password);
+            return session;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+        }
+    }
+} 
+
+const authService = new AuthService();
+
+
+export default AuthService;
